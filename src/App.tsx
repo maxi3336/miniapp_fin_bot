51a0ff9b0f4364b6d02f558b5524fbe4babe6b76
Max <maxi.pletenev@gmail.com>
2025-01-07 14:27:07 +0500

    expense

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,52 @@
-import { useState } from "react";
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { ExpenseCard } from "./components/expense/ExpenseCard";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export function App() {
   return (
-    <div>
-      <Button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </Button>
+    <div className="w-screen h-screen p-6 pb-20 overflow-auto">
+      <Tabs defaultValue="expense" className="w-full">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="expense">Расход</TabsTrigger>
+          <TabsTrigger value="income">Доход</TabsTrigger>
+        </TabsList>
+        <TabsContent value="expense">
+          <ExpenseCard />
+        </TabsContent>
+        <TabsContent value="income">
+          <Card>
+            <CardHeader>
+              <CardTitle>Доход</CardTitle>
+              <CardDescription>
+                Change your password here. After saving, you'll be logged out.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="current">Current password</Label>
+                <Input id="current" type="password" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="new">New password</Label>
+                <Input id="new" type="password" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>Save password</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+      </Tabs>
     </div>
   );
 }
---
+++ src/components/expense/ExpenseCard.tsx
@@ -0,0 +1,231 @@
+import { useForm } from "react-hook-form";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { cn } from "@/lib/utils";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import { ru } from "date-fns/locale";
+
+const formSchema = z.object({
+  date: z.date({ required_error: "Дата обязательна!" }),
+  category: z.string({ required_error: "Категория обязательна!" }).min(0, {
+    message: "Категория не может быть пустой",
+  }),
+  amount: z.number({ required_error: "Сумма обязательна!" }).min(0, {
+    message: "Сумма не может быть пустой",
+  }),
+  account: z.string({ required_error: "Счёт обязателен!" }).min(0, {
+    message: "Счёт не может быть пустой",
+  }),
+  comment: z
+    .string({ required_error: "Комментарий обязателен!" })
+    .min(0, {
+      message: "Комментарий не может быть пустой",
+    })
+    .optional(),
+});
+
+export function ExpenseCard() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <Card>
+          <CardHeader>
+            <CardTitle>Расход</CardTitle>
+            <CardDescription>
+              С помощью данной формы записывается расход в таблицу.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-4">
+            <FormField
+              control={form.control}
+              name="date"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel>Дата</FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          variant={"outline"}
+                          className={cn(
+                            "w-full pl-3 text-left font-normal",
+                            !field.value && "text-muted-foreground"
+                          )}
+                        >
+                          {field.value ? (
+                            format(field.value, "PPP", { locale: ru })
+                          ) : (
+                            <span>Выбери дату</span>
+                          )}
+                          <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                      <Calendar
+                        mode="single"
+                        selected={field.value}
+                        onSelect={field.onChange}
+                        disabled={(date) =>
+                          date > new Date() || date < new Date("1900-01-01")
+                        }
+                        locale={ru}
+                      />
+                    </PopoverContent>
+                  </Popover>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="account"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Категория</FormLabel>
+
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Выбери категорию" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="main">Кредитная СберКарта</SelectItem>
+                      <SelectItem value="m@google.com">
+                        Тинькофф Платинум
+                      </SelectItem>
+                      <SelectItem value="m@support.com">
+                        Основной (Максим)
+                      </SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="amount"
+              render={({ field: { value, onChange, ...rest } }) => (
+                <FormItem>
+                  <FormLabel>Сумма</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="number"
+                      value={value}
+                      onChange={({ target: { value } }) =>
+                        onChange(value ? Number(value) : undefined)
+                      }
+                      {...rest}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="account"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Счёт</FormLabel>
+
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Выбери счёт" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="main">Кредитная СберКарта</SelectItem>
+                      <SelectItem value="m@google.com">
+                        Тинькофф Платинум
+                      </SelectItem>
+                      <SelectItem value="m@support.com">
+                        Основной (Максим)
+                      </SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="comment"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Комментарий</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      placeholder="Что-то про трату"
+                      className="resize-none"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </CardContent>
+        </Card>
+        <div className="absolute bottom-0 left-0 p-4 w-full">
+          <Button size="lg" className="w-full">
+            Записать расход
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+}
