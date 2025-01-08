8ddf1d3b098ed4fbd93188058bfc01d08ab9fa3f
Max <maxi.pletenev@gmail.com>
2025-01-08 16:18:05 +0500

    pwa?

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,41 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate", // Автообновление service worker
+      includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.png"], // Ассеты для PWA
+      manifest: {
+        name: "Finance",
+        short_name: "Fin",
+        description: "A Progressive Finance Web App built with Vite and React",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   base: "/",
   resolve: {
     alias: {
